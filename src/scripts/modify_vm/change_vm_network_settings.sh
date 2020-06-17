40625dbe5b126fbce6ddab8c9ffd6fe9aeb9425d
ESPGranEdu <raplhwigum@gmail.com>
2020-06-17 12:13:46 +0200

    Update change_vm_network_settings.sh

--- src/scripts/modify_vm/change_vm_network_settings.sh
+++ src/scripts/modify_vm/change_vm_network_settings.sh
@@ -24,15 +24,33 @@ vm_nic_config=$(echo "$vm_nic_line" | grep -Eo "Attachment:\s[a-zA-Z0-9' ]*" | a
 vm_nic_config=${vm_nic_config:-disabled}
 vm_nic=$(echo "$vm_nic_line" | grep -Eo "^NIC\s[0-9]:" | awk -F: '{print tolower($1)}' | tr -d " ")
 vm_nic_num=${vm_nic/nic/} # Remove "nic" word
+if ps ax -o command | grep -v grep | grep -q "$vm"; then
+	vboxmanage_command="controlvm"
+else
+	vboxmanage_command="modifyvm"
+fi
 
 # Display basic info
 echo -e "${light_blue}Network configuration of ${light_yellow}${vm}'s ${light_green}${vm_nic^^}${reset}:"
+echo -e "${light_blue}Actual NIC config: $vm_nic_config"
 read -rp "$(display_info --warning "Change the Network config of ${light_yellow}$vm_nic${reset}?(Y/n): ")" ans
 
 if [[ "${ans,,}" == @(yes|y|) ]]; then
-		nic_config=$(echo -e "disabled\nnat\nnat network\nbridged\nintnet\nhostonly\ngeneric" | fzf --prompt "Select NIC mode: ")
-		display_info --info "${light_blue}Setting ${light_yellow}${vm_nic^^}${light_blue} to ${light_yellow}$nic_config${reset}"
-		vboxmanage modifyvm "$vm" --"$vm_nic" "$nic_config" &>/dev/null
+
+	[[ "$vm_nic_config" == "disabled" && "$vboxmanage_command" == "controlvm" ]] &&
+        { display_info --warning "In order to activate a NIC, the VM must be switched off" && return 1;}
+
+	# Check if the VM is running, if it is use "controlvm" instead of "modifyvm"
+	nic_config=$(echo -e "none\nnat\nnat network\nbridged\nintnet\nhostonly\ngeneric" | fzf --prompt "Select NIC mode: ")
+	display_info --info "${light_blue}Setting ${light_yellow}${vm_nic^^}${light_blue} to ${light_yellow}$nic_config${reset}"
+
+	if [[ "$vboxmanage_command" == "modifyvm" ]]; then
+		vboxmanage "$vboxmanage_command" "$vm" --"$vm_nic" "${nic_config//\ /}" #&>/dev/null
+	else
+		display_info --info "${light_blue}Unpluging virtual cable${reset}"
+		vboxmanage "$vboxmanage_command" "$vm" "$vm_nic" "${nic_config//\ /}" #&>/dev/null
+		display_info --info "${light_blue}Pluging virtual cable${reset}"
+	fi
 else
-        return 0
+        return 1
 fi
